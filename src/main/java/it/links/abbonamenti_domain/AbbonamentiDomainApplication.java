b2e16f8f831fbd0f8bbf2ff6ae51bccdc047577f
AngRafFrancavilla <francavillaangeloraffaele@gmail.com>
2025-04-09 10:15:54 +0200

    Importo Progetto

---
+++ src/main/java/it/links/abbonamenti_domain/AbbonamentiDomainApplication.java
@@ -0,0 +1,13 @@
+package it.links.abbonamenti_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbonamentiDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbbonamentiDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/links/abbonamenti_domain/AbbonamentiDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package it.links.abbonamenti_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbbonamentiDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
